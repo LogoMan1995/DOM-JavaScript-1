b7062569c31523374bcb640e5d44997e48f7a1dd
David <david.grigorian777@gmail.com>
2024-09-28 15:14:12 +0400

    finish

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-document.querySelector(".grid").addEventListener("mouseover", (e) => {
-    if (e.target.tagName === "IMG") {
-        let myElement = document.createElement('div');
-        myElement.className = "preview";
-        e.target.parentNode.appendChild(myElement);
-
-        let myImg = document.createElement('img');
-        let locImg = e.target.src;
-
-        myImg.src = locImg.substring(0, locImg.length - 7) + ".jpg";
-        myElement.appendChild(myImg);
-
-        
-        const handleMouseOut = () => {
-            let myNode = e.target.parentNode.querySelector("div.preview");
-            if (myNode) {
-                myNode.remove();
-            }
-            
-            e.target.removeEventListener('mouseout', handleMouseOut);
-        };
-
-        
-        e.target.addEventListener('mouseout', handleMouseOut);
-    }
-}, false);
+document.querySelector('.grid').addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+    if (e.target.tagName === 'IMG') {
+  
+      let myElement = document.createElement('div');
+      myElement.className = 'preview';
+      e.target.parentNode.appendChild(myElement);
+  
+      let myImg = document.createElement('img');
+      let imgLoc = e.target.src;
+      myImg.src = imgLoc.substr(0, imgLoc.length-7) + '.jpg';
+  
+      myElement.style.left = e.offsetX + 15 + 'px';
+      myElement.style.top = e.offsetY + 15 + 'px';
+  
+      myElement.appendChild(myImg);
+  
+      e.target.addEventListener('mouseout', function handler(d) {
+        let myNode = d.target.parentNode.querySelector('div.preview');
+        myNode.parentNode.removeChild(myNode);
+        e.target.removeEventListener('mouseout', handler, false);
+      }, false);
+  
+      e.target.addEventListener('mousemove', function(f) {
+        myElement.style.left = f.offsetX + 15 + 'px';
+        myElement.style.top = f.offsetY + 15 + 'px';
+      });    
+  
+    } 
+  }, false); 
