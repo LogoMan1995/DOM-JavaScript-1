137c0fff96ee613085a5f44d34bf7326c05fd931
David <david.grigorian777@gmail.com>
2024-09-28 14:28:57 +0400

    fix bug

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 document.querySelector(".grid").addEventListener("mouseover", (e) => {
-    if (e.target.tagName === "IMG") { // Исправлено на ===
+    if (e.target.tagName === "IMG") {
         let myElement = document.createElement('div');
         myElement.className = "preview";
         e.target.parentNode.appendChild(myElement);
 
         let myImg = document.createElement('img');
         let locImg = e.target.src;
-        
-        // Исправлено на substring и корректный индекс
-        myImg.src = locImg.substring(0, locImg.length - 7) + ".jpg"; 
-        myElement.appendChild(myImg); // Добавляем myImg в myElement
-    }
-});
 
+        myImg.src = locImg.substring(0, locImg.length - 7) + ".jpg";
+        myElement.appendChild(myImg);
+
+        
+        const handleMouseOut = () => {
+            let myNode = e.target.parentNode.querySelector("div.preview");
+            if (myNode) {
+                myNode.remove();
+            }
+            
+            e.target.removeEventListener('mouseout', handleMouseOut);
+        };
+
+        
+        e.target.addEventListener('mouseout', handleMouseOut);
+    }
+}, false);
